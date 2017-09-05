b8830789bc87144271ae5f19512b06a114c31137
Curtis Rueden <ctrueden@wisc.edu>
2017-09-05 11:06:59 -0500

    Travis: fix javadoc destination

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-javadoc.sh
-sh travis-javadoc.sh SciJava $encrypted_f800cb8bd57b_key $encrypted_f800cb8bd57b_iv
+sh travis-javadoc.sh Java3D $encrypted_f800cb8bd57b_key $encrypted_f800cb8bd57b_iv
